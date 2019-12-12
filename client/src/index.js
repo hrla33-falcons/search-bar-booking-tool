73932a0bbe0588562d3b8c09ab972ca8ac0a0223
liu-austin <austinliu279@gmail.com>
2019-12-12 11:17:31 -0800

    changes render div

--- client/src/index.js
+++ client/src/index.js
@@ -6,4 +6,4 @@ import { Provider } from 'react-redux';
 import { store } from './redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('app'));
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('app1'));
